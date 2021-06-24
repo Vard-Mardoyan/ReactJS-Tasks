3e9c52cf9ca839116d8a99554437c0a574fe75c5
Vard Mardoyan <vardmardoyan77@gmail.com>
2021-06-25 02:03:23 +0400

    simple dropdown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Calculator from "./components/Calculator/calculator";
-
+import UserInfo from "./components/UserInfo/userinfo";
+import Counter from "./components/Counter/counter";
+import Cards from "./components/Unmounting/unmounting";
+import SearchDropdown from "./components/Dropdown/dropdown";
+import countries from "./components/Dropdown/countries";
+import Museum from "./components/Museum/museum";
+import TicTacToe from "./components/Game/tic-tac-toe";
+import Slider from "./components/Slider/Slider";
 function App() {
   return (
     <div className="App">
     <h1>Calculator</h1>
       <Calculator />
-
+<h1>Counter</h1>
+<Counter />
+<h1>Dropdown</h1>
+<Dropdown />
     </div>
   );
 }
---
+++ src/components/Counter/counter.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+			inpValue: "",
+			stepBy: 1,
+			min: 0,
+			max: 0,
+		};
+	}
+
+	handleMinVal = (e) => {
+		this.setState({
+			min: Number(e.target.value),
+		});
+	};
+	handleMaxVal = (e) => {
+		this.setState({
+			max: e.target.value === "" ? e.target.value : Number(e.target.value),
+			// count: Math.max(this.state.min + this.state.stepBy, this.state.count - this.state.min),
+		});
+	};
+
+	handleIncrement = () => {
+		let count =
+			this.state.max === 0
+				? this.state.count + this.state.stepBy
+				: Math.min(this.state.max, this.state.count + this.state.stepBy);
+		this.setState({ count });
+	};
+	handleDecrement = () => {
+		let count = Math.max(this.state.min, this.state.count - this.state.stepBy);
+		this.setState({ count });
+	};
+
+	handleInputUpdate = (e) => {
+		this.setState({
+			stepBy: Number(e.target.value),
+		});
+	};
+
+	handleReset = () => {
+		let count = 0;
+		let min = 0;
+		let max = 0;
+		let inpValue = "";
+
+		this.setState({ count, min, max, inpValue });
+	};
+
+	render() {
+		console.log(this.state.count);
+
+		return (
+			<div>
+				<p>
+					Counte:
+					{this.state.count}
+				</p>
+				<input type="number" onChange={this.handleInputUpdate} />
+				<button onClick={this.handleIncrement}>+</button>
+				<button onClick={this.handleDecrement}>-</button>
+				<button onClick={this.handleReset}>Reset</button>
+
+				<p>Min: {this.state.min}</p>
+				<input
+					value={this.state.min}
+					type="number"
+					onChange={this.handleMinVal}
+				/>
+				<p>Max: {this.state.max}</p>
+				<input
+					value={this.state.max}
+					type="number"
+					onChange={this.handleMaxVal}
+				/>
+			</div>
+		);
+	}
+}
+
+export default Counter;
